ffc7fdc1d2f502e651fa52032c4087d574b05357
buhklab <80883507+buhklab@users.noreply.github.com>
2021-03-26 08:17:03 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+function initialize(){
+    var status = "* Offline *";
+    if(navigator.onLine){
+        status = "* Offline *";
+        retrieveContacts();
+    }else{
+        const localStorage = windwo.localStorage;
+        if(localStorage){
+            const contacts = localStorage.getItem("contacts");
+            if(contacts){
+                displayContacts(JSON.parse(contacts));
+            }
+        }
+    }
+
+    document.getElementById("status").innerHTML = status;
+
+    document.body.addEventListener(
+        "online", function(){
+            document.getElementById("status").innerHTML = "Online";
+
+        }, false);
+    
+    document.body.addEventListener(
+        "offline", function(){
+            document.getElementById("status").innerHTML = "offline"
+
+        }, false
+    );
+
+    }  
+    
+
+
+
+
+function retrieveContacts(){
+    const xhr = new XMLHttpRequest();
+    const url = "contacts.json";
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            var contacts = JSON.parse(xhr.response).contacts;
+            displayContacts(contacts);
+            const localStorage = window.localStorage;
+            if(localStorage){
+               localStorage.setItem("contacts", JSON.stringify(contacts));
+            }
+
+        }
+    };
+    xhr.open("get", url);
+    xhr.send();
+
+}
+
+function displayContacts(contacts){
+    var tcontent = document.getElementById("tContent");
+    var row = tcontent.insertRow();
+    var nameCell = row.insertCell();
+    nameCell.setAttribute('data-label', "Name");
+    nameCell.innerHTML = contacts.name;
+    var addressCell = row.insertCell();
+    addressCell.setAttribute('data-label', "Address");
+    addressCell.innerHTML = contacts.address;
+    var emailCell = row.insertCell();
+    emailCell.setAttribute('data-label', "Email");
+    emailCell.innerHTML = contacts.email;
+    
+
+}
